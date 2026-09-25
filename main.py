7b12e078c95d31128b57d26fdecde43d3c97573e
winlattkyansin67-tech <winlattkyansin67@gmail.com>
2026-09-26 00:06:20 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import telebot
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running!")
+
+def run_http_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+threading.Thread(target=run_http_server, daemon=True).start()import telebot
 import requests
 
 TOKEN = '8507984706:AAHI9o5wHyVXfgLaBqf9CFeH1zgcTvfsmuo'
